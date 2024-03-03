b9663edf6961ff74601d0a6ce582fc9bd9f96690
Mathur <takmathur@deloitte.com>
2024-03-03 20:30:18 +0530

    requirements added

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = 'mlProject'
+project_name = 'mlproject'
 
 list_of_files = [
     ".github/workflows/.gitkeep",
